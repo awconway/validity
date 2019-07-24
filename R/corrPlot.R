5a6a8cc87fc380380451933525441368a7eac3ce
Aaron Conway <aaronconwayphd@gmail.com>
2019-07-24 13:38:26 -0400

    suppress message geom_smooth

--- R/corrPlot.R
+++ R/corrPlot.R
@@ -5,7 +5,7 @@
 
 corrPlot <- function(x,y){
   options(warn = -1)
-  ggplot2::ggplot(data = validity::data, ggplot2::aes(x={{ x }}, y={{ y }})) +
+  p <- ggplot2::ggplot(data = validity::data, ggplot2::aes(x={{ x }}, y={{ y }})) +
     ggplot2::geom_point()+
     ggplot2::annotate(geom = "text", x=50, y=22, 
                       label="Pearson's correlation = -0.75; p<0.001", 
@@ -13,4 +13,5 @@ corrPlot <- function(x,y){
     ggplot2::geom_smooth()+
     ggplot2::scale_x_continuous(limits = c(15, 75), expand = c(0, 0))+
     ggplot2::scale_y_continuous(limits = c(-2, 30), expand = c(0, 0))
+  suppressMessages(print(p))
 }
