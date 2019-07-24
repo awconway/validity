4f939ee96f9a49ca47cdba352b9eb95ee238388c
Aaron Conway <aaronconwayphd@gmail.com>
2019-07-24 14:13:36 +0000

    corrPlot function added

---
+++ R/corrPlot.R
@@ -0,0 +1,16 @@
+#' Plot linear regression model
+#' @rdname corrPlot
+#' @name corrPlot
+#' @export
+
+corrPlot <- function(x,y){
+  options(warn = -1)
+  ggplot2::ggplot(data = validity::data, ggplot2::aes(x={{ x }}, y={{ y }})) +
+    ggplot2::geom_point()+
+    ggplot2::annotate(geom = "text", x=50, y=22, 
+                      label="Pearson's correlation = -0.75; p<0.001", 
+                      colour = "Red") +
+    ggplot2::geom_smooth()+
+    ggplot2::scale_x_continuous(limits = c(15, 75), expand = c(0, 0))+
+    ggplot2::scale_y_continuous(limits = c(-2, 30), expand = c(0, 0))
+}
